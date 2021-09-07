e9669373e9a8f8572e4d8e268ae070f599bc22ad
Łukasz Kamiński <lkamins7@gmail.com>
2021-09-07 21:16:34 +0200

    Change race to breed

--- src/app/shared/models/pet.model.ts
+++ src/app/shared/models/pet.model.ts
@@ -1,13 +1,13 @@
 export class Pet {
   id: number;
   name: string;
-  race: string;
+  breed: string;
   kind: string;
 
-  constructor(id: number, name: string, race: string, kind: string) {
+  constructor(id: number, name: string, breed: string, kind: string) {
     this.id = id;
     this.name = name;
-    this.race = race;
+    this.breed = breed;
     this.kind = kind;
   }
 }
